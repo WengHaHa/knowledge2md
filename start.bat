cb7965ecb1f2b803183e7db5dabd306334233124
KnowledgeProcessor <knowledge@example.com>
2026-03-08 02:32:01 +0800

    modified:   start.bat

--- start.bat
+++ start.bat
@@ -9,7 +9,7 @@ pip install -r requirements.txt --upgrade
 
 :: 安装前端依赖
 echo 安装前端依赖...
-npm install
+call npm.cmd install
 
 :: 启动后端服务
 echo 启动后端服务...
@@ -21,13 +21,13 @@ timeout /t 3 /nobreak
 
 :: 启动前端开发服务器
 echo 启动前端服务...
-start "前端服务" npm run dev
+start "前端服务" npm.cmd run dev
 
-echo ====================================
+echo ================================
 echo 服务启动完成！
 echo 前端地址: http://localhost:3000
 echo 后端地址: http://localhost:8000
-echo ====================================
+echo ================================
 
 echo 按任意键退出...
 pause
